c4c4009b7fb676ef2092dcacc8d32e5067bcabb2
grayystar <stargrayyy88@gmail.com>
2026-05-05 15:35:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,36 +4,97 @@ function salvar(){
 localStorage.setItem("roupas", JSON.stringify(roupas));
 }
 
-function adicionarRoupa(){
+// TROCAR TELA
+function ir(tela){
+document.querySelectorAll(".tela").forEach(t=>{
+t.classList.remove("ativa");
+});
+document.getElementById(tela).classList.add("ativa");
 
-let nome = prompt("Digite nome da roupa:");
-
-if(nome){
-roupas.push(nome);
-salvar();
-resultado.innerHTML = "✅ Roupa adicionada!";
+if(tela=="closet") ver();
 }
 
-}
+// ADICIONAR ROUPA
+function adicionar(){
 
-function verCloset(){
+let nome = document.getElementById("nome").value;
+let categoria = document.getElementById("categoria").value;
+let cor = document.getElementById("cor").value;
+let file = document.getElementById("foto").files[0];
 
-if(roupas.length == 0){
-resultado.innerHTML = "👚 Closet vazio!";
+if(!nome || !cor){
+alert("Preencha tudo!");
 return;
 }
 
-resultado.innerHTML = roupas.map(r => "👗 " + r).join("<br>");
+// CRIA UM LEITOR DE FOTO
+let reader = new FileReader();
+
+reader.onload = function(e){
+
+// SALVA NO FIREBASE
+db.collection("roupas").add({
+nome: nome,
+categoria: categoria,
+cor: cor,
+foto: e.target.result // FOTO AQUI 📸
+});
+
+alert("Roupa salva com foto!");
 }
 
-function montarLook(){
+// SE TIVER FOTO
+if(file){
+reader.readAsDataURL(file);
+}else{
+// SEM FOTO
+db.collection("roupas").add({nome,categoria,cor});
+alert("Roupa salva!");
+}
 
-if(roupas.length == 0){
-resultado.innerHTML = "Adicione roupas primeiro!";
+}
+
+// VER CLOSET
+function ver(){
+
+let lista = document.getElementById("lista");
+
+db.collection("roupas").get().then((dados)=>{
+
+lista.innerHTML = "";
+
+dados.forEach((doc)=>{
+let r = doc.data();
+
+lista.innerHTML += `
+<div style="background:#fff0f6; padding:10px; margin:10px; border-radius:10px;">
+<b>${r.nome}</b><br>
+${r.categoria} - ${r.cor}
+
+${r.foto ? `<img src="${r.foto}" style="width:100%; border-radius:10px;">` : ""}
+
+</div>
+`;
+
+});
+
+});
+}
+
+// GERAR LOOK
+function gerarLook(){
+
+if(roupas.length==0){
+document.getElementById("lookResultado").innerHTML="Adicione roupas!";
 return;
 }
 
-let sorteio = roupas[Math.floor(Math.random()*roupas.length)];
+let r = roupas[Math.floor(Math.random()*roupas.length)];
 
-resultado.innerHTML = "✨ Look do dia:<br><br>👗 " + sorteio;
+document.getElementById("lookResultado").innerHTML = `
+<div class="item">
+✨ ${r.nome}<br>
+${r.categoria}
+</div>
+`;
 }
